384ca789d590e4ec46cf52c27f27dd7151fc5a06
Gonzalo Ponce <mariogonzalop09@gmail.com>
2024-08-02 18:46:16 -0300

    modificado el login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,49 +1,89 @@
-import { useState, useContext } from 'react';
-import { AuthContext } from '../../context/AuthContext';
-import './Login.css';
+import { Link } from "react-router-dom";
+import "./Login.css";
+import { useState } from "react";
 
 function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const { login, error } = useContext(AuthContext);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (!username || !password) {
+            setError("Por favor, completa todos los campos.");
+            return;
+        }
+
         try {
-            await login(username, password);
-            window.location.href = '/profile';
+            const response = await fetch("https://sandbox.academiadevelopers.com/api-auth/", {
+                method: "POST",
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+                headers: {
+                    "Content-type": "application/json",
+                },
+                credentials: "include",
+            });
+
+            if (response.status === 401) {
+                setError("Usuario o contraseña incorrectos");
+            } else if (response.status === 200) {
+                const data = await response.json();
+                localStorage.setItem("token", data.token);
+                window.location.href = "/profile";
+            } else {
+                setError("Error desconocido. Por favor, inténtalo de nuevo.");
+            }
         } catch (error) {
-            alert(error.message);
+            console.error(error);
+            setError("Error de red. Por favor, inténtalo de nuevo.");
         }
     };
 
     return (
-        <form className="formulario" onSubmit={handleSubmit}>
-            <h1>Login</h1>
-            <div className="contenedor">
-                <div className="input-contenedor">
-                    <i className="icon mail"></i>
-                    <input
-                        type="text"
-                        placeholder="Nombre de Usuario"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div className="input-contenedor">
-                    <i className="icon key"></i>
-                    <input
-                        type="password"
-                        placeholder="Contraseña"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <input type="submit" value="Login" className="button" />
-                <p>Al registrarse acepta nuestras condiciones de uso y Politicas de seguridad</p>
-                <p>¿No tienes una cuenta? <a className="link" href="registro.html">Registrate</a></p>
+        <>
+            <div className="cuerpo">
+                <form className="formulario" onSubmit={handleSubmit}>
+                    <h1>Login</h1>
+                    <div className="contenedor">
+                        {error && <p className="error-message">{error}</p>}
+                        <div className="input-contenedor">
+                            <ion-icon name="mail"></ion-icon>
+                            <input
+                                type="text"
+                                placeholder="Nombre de Usuario"
+                                value={username}
+                                onChange={handleUsernameChange}
+                            />
+                        </div>
+
+                        <div className="input-contenedor">
+                            <ion-icon name="key"></ion-icon>
+                            <input
+                                type="password"
+                                placeholder="Contraseña"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                        </div>
+                        <input type="submit" value="Login" className="button" />
+                        <p>Al registrarse acepta nuestras condiciones de uso y Políticas de seguridad</p>
+                        <p>¿No tienes una cuenta? <a className="link" href="registro.html">Regístrate</a></p>
+                    </div>
+                </form>
             </div>
-        </form>
+        </>
     );
 }
 
